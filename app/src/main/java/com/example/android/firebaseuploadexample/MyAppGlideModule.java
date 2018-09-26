9455f9d483ecaffa221e3bed75a1b13e7d993efc
Tarek <tarek.buhdeima@gmail.com>
2018-09-27 01:17:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/firebaseuploadexample/MyAppGlideModule.java
@@ -0,0 +1,22 @@
+package com.example.android.firebaseuploadexample;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
+
+/**
+ * Created by Tarek on 25-Sep-18.
+ */
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+
+    @Override
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+        builder.setDefaultRequestOptions(new RequestOptions().format(DecodeFormat.PREFER_ARGB_8888));
+    }
+}
